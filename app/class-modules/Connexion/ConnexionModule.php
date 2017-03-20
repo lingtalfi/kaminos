eca24f0398e76e367bc0f3034cd28f1acc1c45a2
lingtalfi <lingtalfi@gmail.com>
2017-03-20 08:07:12 +0100

    update

--- app/class-modules/Connexion/ConnexionModule.php
+++ app/class-modules/Connexion/ConnexionModule.php
@@ -6,19 +6,34 @@ namespace Connexion;
 
 use Kamille\Module\Exception\ModuleException;
 use Kamille\Module\ModuleInterface;
+use Kamille\Utils\StepTracker\StepTrackerAwareInterface;
+use Kamille\Utils\StepTracker\StepTrackerInterface;
 
-class ConnexionModule implements ModuleInterface
+class ConnexionModule implements ModuleInterface, StepTrackerAwareInterface
 {
 
+    /**
+     * @var StepTrackerInterface $stepTracker
+     */
+    private $stepTracker;
 
     public function install()
     {
         a("connexion module install");
+
+
+        $this->stepTracker->startStep("connexion module install");
+        $this->stepTracker->startStep("connexion module install");
     }
 
     public function uninstall()
     {
         a("connexion module uninstall");
+    }
+
+    public function setStepTracker(StepTrackerInterface $stepTracker)
+    {
+        $this->stepTracker=$stepTracker;
     }
 
 

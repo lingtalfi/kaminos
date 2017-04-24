505ea45cc1903bb674153772aa870d665fbd5895
lingtalfi <lingtalfi@gmail.com>
2017-04-24 17:28:47 +0200

    update

--- app/config/laws/test.conf.php
+++ app/config/laws/test.conf.php
@@ -1,11 +1,86 @@
 <?php
 
 
+use Kamille\Architecture\ApplicationParameters\ApplicationParameters;
 
+$prefixUri = "/theme/" . ApplicationParameters::get("theme");
+$imgPrefix = $prefixUri . "/production";
 $conf = [
     "layout" => [
         "tpl" => "admin/default",
     ],
     "widgets" => [
+        "sidebar.navTitle" => [
+            "tpl" => "NullosAdmin/NavTitle/default",
+            "conf" => [
+                "link" => "index.html",
+                "iconClass" => "fa fa-paw",
+                "title" => "NullosAdmin",
+            ],
+        ],
+        "sidebar.menuProfileQuickInfo" => [
+            "tpl" => "NullosAdmin/MenuProfileQuickInfo/default",
+            "conf" => [
+                "imgSrc" => $imgPrefix . '/images/ling.jpg',
+                "imgAlt" => "...",
+                "welcomeText" => "Welcome,",
+                "userName" => "John Doe",
+            ],
+        ],
+        "maincontent.a" => [
+            "grid" => "1/3",
+            "tpl" => "HtmlCode/default",
+            "conf" => [
+                "html" => "content A",
+            ],
+        ],
+        "maincontent.b" => [
+            "grid" => "1/3",
+            "tpl" => "HtmlCode/default",
+            "conf" => [
+                "html" => "content B",
+            ],
+        ],
+        "maincontent.c" => [
+            "grid" => "1/3",
+            "tpl" => "HtmlCode/default",
+            "conf" => [
+                "html" => "content C",
+            ],
+        ],
+        "maincontent.d" => [
+            "grid" => "1/3",
+            "tpl" => "HtmlCode/default",
+            "conf" => [
+                "html" => "content D",
+            ],
+        ],
+        "maincontent.e" => [
+            "grid" => "2/3-1",
+            "tpl" => "HtmlCode/default",
+            "conf" => [
+                "html" => "content E",
+            ],
+        ],
+        "maincontent.f" => [
+            "grid" => "1/2",
+            "tpl" => "HtmlCode/default",
+            "conf" => [
+                "html" => "content F",
+            ],
+        ],
+        "maincontent.g" => [
+            "grid" => "1/2",
+            "tpl" => "HtmlCode/default",
+            "conf" => [
+                "html" => "content G",
+            ],
+        ],
+    ],
+    "grid" => ["maincontent"],
+    "positions" => [
+        "*" => function ($s) {
+            return '<div style="color: red">' . $s . '</div>';
+        },
     ],
 ];
--- app/www/no.php
+++ app/www/no.php
@@ -3,6 +3,10 @@
 
 use Kamille\Architecture\ApplicationParameters\ApplicationParameters;
 use Kamille\Ling\Z;
+use Kamille\Mvc\LayoutProxy\LawsLayoutProxy;
+use Kamille\Mvc\WidgetDecorator\GridWidgetDecorator;
+use Kamille\Mvc\WidgetDecorator\PositionWidgetDecorator;
+use Kamille\Utils\Laws\LawsUtil;
 
 require_once __DIR__ . "/../boot.php";
 require_once __DIR__ . "/../init.php";
@@ -10,12 +14,15 @@ require_once __DIR__ . "/../init.php";
 
 
 
-// the url is: http://kaminos/no.php?pou=6
 
-
-a(Z::uri()); // same uri, no query string                                                   /no.php
-a(Z::uri(null, [], false)); // same uri, with query string                      /no.php?pou=6
-a(Z::uri(null, ["doo" => 7], false)); // same uri, merging params               /no.php?pou=6&doo=7
-a(Z::uri(null, ["doo" => 7], true)); // same uri, replacing params              /no.php?doo=7
-a(Z::uri(null, ["doo" => 7], true, true)); // prefix with host         http://kaminos/no.php?doo=7
-a(Z::uri("/myown", ['foo'], true, true));  // own uri                  http://kaminos/myown?0=foo
+$viewId = "test";
+$options = [];
+$config = [];
+$decorator = GridWidgetDecorator::create();
+$layoutProxy = LawsLayoutProxy::create()
+    ->addDecorator(PositionWidgetDecorator::create())
+    ->addDecorator($decorator)
+;
+echo LawsUtil::create()
+    ->setLawsLayoutProxy($layoutProxy)
+    ->renderLawsViewById($viewId, $config, $options);
